d02f001e49cebd0ed7bfa1b2cc06e4485f36a401
Tim Yates <tim@gradle.com>
2021-12-06 11:07:46 +0000

    Deque version too

--- lib/src/main/kotlin/com/bloidonia/advent/day06/Day06Optimized.kt
+++ lib/src/main/kotlin/com/bloidonia/advent/day06/Day06Optimized.kt
@@ -1,23 +1,48 @@
 package com.bloidonia.advent.day06
 
 import com.bloidonia.advent.readText
+import kotlin.collections.ArrayDeque
 
-class OptimizedPopulation(private val population: LongArray) {
+class ArrayPopulation(private val population: LongArray) {
     fun generation() = (population.copyOfRange(1, 9) + population[0]).let {
         it[6] += it[8]
-        OptimizedPopulation(it)
+        ArrayPopulation(it)
     }
 
     fun size() = population.sum()
 }
 
-fun String.toPop2() =
-    OptimizedPopulation(this.split(",").map { it.toInt() }.fold(LongArray(9) { 0L }) { arr, v -> arr[v]++; arr })
+fun String.toArrayPopulation() =
+    ArrayPopulation(this.split(",").map { it.toInt() }.fold(LongArray(9) { 0L }) { arr, v -> arr[v]++; arr })
+
+class DequeuePopulation(private val population: ArrayDeque<Long>) {
+    fun generation() = ArrayDeque(population).run {
+        removeFirst().let {
+            addLast(it)
+            this[6] += it
+            DequeuePopulation(this)
+        }
+    }
+
+    fun size() = population.sum()
+}
+
+fun String.toDequeuePopulation() =
+    DequeuePopulation(this.split(",").map { it.toInt() }.fold(ArrayDeque(LongArray(9) { 0L }.toList())) { arr, v -> arr[v]++; arr })
 
 fun main() {
-    val initial = readText("/day06input.txt").toPop2()
+    // array
+    val initial = readText("/day06input.txt").toArrayPopulation()
 
     generateSequence(initial) { it.generation() }.run {
+        println(drop(80).first().size())
+        println(drop(256).first().size())
+    }
+
+    // ArrayDequeue
+    val dqInitial = readText("/day06input.txt").toDequeuePopulation()
+
+    generateSequence(dqInitial) { it.generation() }.run {
         println(drop(80).first().size())
         println(drop(256).first().size())
     }
