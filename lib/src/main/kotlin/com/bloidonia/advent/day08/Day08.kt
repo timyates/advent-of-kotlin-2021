81f94f6187246e506550783436cb34227479416a
Tim Yates <tim@gradle.com>
2021-12-08 09:58:15 +0000

    Extract method for intersection size

--- lib/src/main/kotlin/com/bloidonia/advent/day08/Day08.kt
+++ lib/src/main/kotlin/com/bloidonia/advent/day08/Day08.kt
@@ -2,71 +2,73 @@ package com.bloidonia.advent.day08
 
 import com.bloidonia.advent.readList
 
-data class Digit(val wires: Set<Char>)
+data class Digit(val wires: Set<Char>) {
+    fun intersections(other: Digit) = wires.intersect(other.wires).size
+}
 data class Line(val digits: List<Digit>, val display: List<Digit>) {
     fun numberKnown(): Int = display.count { it.wires.size in listOf(2, 3, 4, 7) }
     fun derive(): String {
-        val resolved = Array<Digit?>(10) { null }
+        val resolved = Array(10) { Digit(setOf()) }
         resolved[1] = digits.first { it.wires.size == 2 }
         resolved[4] = digits.first { it.wires.size == 4 }
         resolved[7] = digits.first { it.wires.size == 3 }
         resolved[8] = digits.first { it.wires.size == 7 }
         resolved[0] = digits.first {
             !resolved.contains(it)
-                    && it.wires.containsAll(resolved[1]!!.wires)
-                    && it.wires.containsAll(resolved[7]!!.wires)
-                    && it.wires.intersect(resolved[4]!!.wires).size == 3
                     && it.wires.size == 6
+                    && it.wires.containsAll(resolved[1].wires)
+                    && it.wires.containsAll(resolved[7].wires)
+                    && it.intersections(resolved[4]) == 3
         }
         resolved[2] = digits.first {
             !resolved.contains(it)
-                    && it.wires.intersect(resolved[1]!!.wires).size == 1
-                    && it.wires.intersect(resolved[7]!!.wires).size == 2
-                    && it.wires.intersect(resolved[4]!!.wires).size == 2
-                    && it.wires.intersect(resolved[0]!!.wires).size == 4
                     && it.wires.size == 5
+                    && it.intersections(resolved[1]) == 1
+                    && it.intersections(resolved[7]) == 2
+                    && it.intersections(resolved[4]) == 2
+                    && it.intersections(resolved[0]) == 4
         }
         resolved[3] = digits.first {
             !resolved.contains(it)
-                    && it.wires.intersect(resolved[1]!!.wires).size == 2
-                    && it.wires.intersect(resolved[7]!!.wires).size == 3
-                    && it.wires.intersect(resolved[4]!!.wires).size == 3
-                    && it.wires.intersect(resolved[0]!!.wires).size == 4
-                    && it.wires.intersect(resolved[2]!!.wires).size == 4
                     && it.wires.size == 5
+                    && it.intersections(resolved[1]) == 2
+                    && it.intersections(resolved[7]) == 3
+                    && it.intersections(resolved[4]) == 3
+                    && it.intersections(resolved[0]) == 4
+                    && it.intersections(resolved[2]) == 4
         }
         resolved[5] = digits.first {
             !resolved.contains(it)
-                    && it.wires.intersect(resolved[1]!!.wires).size == 1
-                    && it.wires.intersect(resolved[7]!!.wires).size == 2
-                    && it.wires.intersect(resolved[4]!!.wires).size == 3
-                    && it.wires.intersect(resolved[0]!!.wires).size == 4
-                    && it.wires.intersect(resolved[2]!!.wires).size == 3
-                    && it.wires.intersect(resolved[3]!!.wires).size == 4
                     && it.wires.size == 5
+                    && it.intersections(resolved[1]) == 1
+                    && it.intersections(resolved[7]) == 2
+                    && it.intersections(resolved[4]) == 3
+                    && it.intersections(resolved[0]) == 4
+                    && it.intersections(resolved[2]) == 3
+                    && it.intersections(resolved[3]) == 4
         }
         resolved[6] = digits.first {
             !resolved.contains(it)
-                    && it.wires.intersect(resolved[1]!!.wires).size == 1
-                    && it.wires.intersect(resolved[7]!!.wires).size == 2
-                    && it.wires.intersect(resolved[4]!!.wires).size == 3
-                    && it.wires.intersect(resolved[0]!!.wires).size == 5
-                    && it.wires.intersect(resolved[2]!!.wires).size == 4
-                    && it.wires.intersect(resolved[3]!!.wires).size == 4
-                    && it.wires.intersect(resolved[5]!!.wires).size == 5
                     && it.wires.size == 6
+                    && it.intersections(resolved[1]) == 1
+                    && it.intersections(resolved[7]) == 2
+                    && it.intersections(resolved[4]) == 3
+                    && it.intersections(resolved[0]) == 5
+                    && it.intersections(resolved[2]) == 4
+                    && it.intersections(resolved[3]) == 4
+                    && it.intersections(resolved[5]) == 5
         }
         resolved[9] = digits.first {
             !resolved.contains(it)
-                    && it.wires.intersect(resolved[1]!!.wires).size == 2
-                    && it.wires.intersect(resolved[7]!!.wires).size == 3
-                    && it.wires.intersect(resolved[4]!!.wires).size == 4
-                    && it.wires.intersect(resolved[0]!!.wires).size == 5
-                    && it.wires.intersect(resolved[2]!!.wires).size == 4
-                    && it.wires.intersect(resolved[3]!!.wires).size == 5
-                    && it.wires.intersect(resolved[5]!!.wires).size == 5
-                    && it.wires.intersect(resolved[6]!!.wires).size == 5
                     && it.wires.size == 6
+                    && it.intersections(resolved[1]) == 2
+                    && it.intersections(resolved[7]) == 3
+                    && it.intersections(resolved[4]) == 4
+                    && it.intersections(resolved[0]) == 5
+                    && it.intersections(resolved[2]) == 4
+                    && it.intersections(resolved[3]) == 5
+                    && it.intersections(resolved[5]) == 5
+                    && it.intersections(resolved[6]) == 5
         }
         return display.joinToString(separator = "") { resolved.indexOf(it).toString() }
     }
